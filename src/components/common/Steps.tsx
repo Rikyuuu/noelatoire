6eecef1c6e9f9873acad5f42704843824980275e
Julien Delissenne <julien.delissenne@atos.net>
2024-12-25 06:51:12 +0100

    fix: stepper

--- src/components/common/Steps.tsx
+++ src/components/common/Steps.tsx
@@ -1,12 +1,35 @@
-const Steps = () => {
+import { StepEnum } from '@/app/enum/StepEnum'
+
+interface StepsProps {
+    currentStep: StepEnum
+}
+
+const Steps = ({ currentStep }: StepsProps) => {
     return (
         <>
             <div>
                 <h2 className='text-2xl font-bold'>Etapes</h2>
                 <ul className='steps steps-vertical lg:steps-horizontal'>
                     <li className='step step-primary'>Nombre</li>
-                    <li className='step'>Noms</li>
-                    <li className='step'>Tirage</li>
+                    <li
+                        className={`step ${
+                            currentStep === StepEnum.PARTICIPANT_NAME_INPUT ||
+                            currentStep === StepEnum.WINNER
+                                ? 'step-primary'
+                                : ''
+                        }`}
+                    >
+                        Noms
+                    </li>
+                    <li
+                        className={`step ${
+                            currentStep === StepEnum.WINNER
+                                ? 'step-primary'
+                                : ''
+                        }`}
+                    >
+                        Tirage
+                    </li>
                 </ul>
             </div>
             <div className='flex w-full flex-col'>
